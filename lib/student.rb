ae5941ae0356bb826348e49b738c661ed0b42662
Kishandth Sivapalasundaram <rodes88@msn.com>
2019-05-25 22:54:29 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -97,4 +97,15 @@ class Student
     DB[:conn].execute(sql, number_of_students)
   end 
   
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+      SELECT * 
+      FROM students
+      WHERE grade = 10
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, number_of_students)
+  end
+  
 end
