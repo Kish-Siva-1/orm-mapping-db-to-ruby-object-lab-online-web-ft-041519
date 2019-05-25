2d5e24a7803eda6de32d6411ec499789af2e46fb
Kishandth Sivapalasundaram <rodes88@msn.com>
2019-05-25 22:58:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -104,11 +104,10 @@ class Student
       WHERE grade = 10
       LIMIT 1
     SQL
-    binding.pry
+
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end.first
-    binding.pry
   end
   
 end
