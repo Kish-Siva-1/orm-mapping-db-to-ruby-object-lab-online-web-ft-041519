5d4f01b57a6d5c592bdfebdec2cac53c45cbb215
Kishandth Sivapalasundaram <rodes88@msn.com>
2019-05-25 21:59:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -52,4 +52,9 @@ class Student
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
+  
+  def self.all_students_in_grade_9
+    sql = "DROP TABLE IF EXISTS students"
+    DB[:conn].execute(sql)
+  end
 end
