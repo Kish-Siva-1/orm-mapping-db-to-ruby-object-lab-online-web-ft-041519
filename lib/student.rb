54c375f468a09cc9d6daaa5c4e7b5a6b2f4c0d97
Kishandth Sivapalasundaram <rodes88@msn.com>
2019-05-25 23:00:03 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -114,13 +114,12 @@ class Student
     sql = <<-SQL
       SELECT * 
       FROM students
-      WHERE grade = 10
-      LIMIT 1
+      GROUP BY id
     SQL
 
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
-    end.first
+    end
   end
   
 end
